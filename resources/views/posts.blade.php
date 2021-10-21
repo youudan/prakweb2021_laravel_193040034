0b391d8c7d8892afab4746745dc6649480966bca
Wijdan Muhammad R <42838967+youudan@users.noreply.github.com>
2021-10-21 09:02:47 +0700

    pertemuan 7, video 3 - n+1 problem

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,14 +3,14 @@
 @section('title', $title)
 
 @section('content')
-<h1 class="mb-5">Halaman Blog Posts</h1>
+<h1 class="mb-5">{{ $title }}</h1>
 
 @foreach ($posts as $post)
 <article class="mb-5 pb-4 border-bottom">
   <h2>
     <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
   </h2>
-  <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/" class="text-decoration-none">{{ $post->category->name }}</a></p>
+  <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
   <p>
     {{ $post->excerpt }}
--- routes/web.php
+++ routes/web.php
@@ -47,11 +47,10 @@ Route::get('/categories', function () {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view(
-        'category',
+        'posts',
         [
-            'title'    => $category->name,
-            'posts'    => $category->posts,
-            'category' => $category->name,
+            'title'    => "Post By Category : $category->name",
+            'posts'    => $category->posts->load('category', 'author'),
         ]
     );
 });
@@ -60,8 +59,8 @@ Route::get('/authors/{author:username}', function (User $author) {
     return view(
         'posts',
         [
-            'title'    => 'User Posts',
-            'posts'    => $author->posts,
+            'title'    => "Post By Author : $author->name",
+            'posts'    => $author->posts->load('category', 'author'),
         ]
     );
 });
