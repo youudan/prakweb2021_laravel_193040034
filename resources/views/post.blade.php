91f19783b9baf20f160b0e70c48866f525dc1e68
Wijdan Muhammad R <42838967+youudan@users.noreply.github.com>
2021-10-28 09:46:44 +0700

    pertemuan 8, video 1 - searching & pagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,7 +10,7 @@
 
       <h1 class="mb-3">{{ $post->title }}</h1>
 
-      <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+      <p>By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
       <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid" alt="{{ $post->category->name }}">
       <article class="my-3 fs-5">
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,21 @@
 @section('title', $title)
 
 @section('content')
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
+
+<div class="row justify-content-center mb-3">
+  <div class="col-md-6">
+    <form action="/posts">
+      @if (request('category'))
+      <input type="hidden" name="category" value="{{request('category')}}">
+      @endif
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+        <button class="btn btn-danger" type="submit">Search</button>
+      </div>
+    </form>
+  </div>
+</div>
 
 @if ($posts->count())
 <div class="card mb-3">
@@ -15,8 +29,8 @@
 
     <p>
       <small class="text-muted">
-        By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in
-        <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+        By. <a href="/posts?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in
+        <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
         {{ $posts[0]->created_at->diffForHumans() }}
       </small>
     </p>
@@ -26,9 +40,7 @@
     <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
   </div>
 </div>
-@else
-<p class="text-center fs-4">No post found.</p>
-@endif
+
 
 <div class="container">
   <div class="row">
@@ -36,14 +48,14 @@
     <div class="col-md-4 mb-3">
       <div class="card">
         <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)">
-          <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a>
+          <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a>
         </div>
         <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
         <div class="card-body">
           <h5 class="card-title">{{ $post->title }}</h5>
           <p>
             <small class="text-muted">
-              By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a>
+              By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a>
               {{ $post->created_at->diffForHumans() }}
             </small>
           </p>
@@ -54,6 +66,13 @@
     </div>
     @endforeach
   </div>
+</div>
+
+@else
+<p class="text-center fs-4">No post found.</p>
+@endif
+<div class="d-flex justify-content-center">
+  {{ $posts->links() }}
 </div>
 
 @endsection
